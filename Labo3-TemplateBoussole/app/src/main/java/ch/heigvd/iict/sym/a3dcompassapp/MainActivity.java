85876b437aef7dd5f5c09b9f21198a440b0860b3
James <james.smith@heig-vd.ch>
2018-12-10 21:15:25 +0100

    Remake main activity

--- Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/MainActivity.java
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/MainActivity.java
@@ -1,15 +1,17 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button nfc;
-    private Button barCode;
+    private Button nfc_qr;
     private Button iBeacon;
     private Button captors;
+    private Button compass;
 
 
     @Override
@@ -17,10 +19,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nfc = (Button) findViewById(R.id.toNFCActivity);
-        barCode = (Button) findViewById(R.id.toBarCodeActivity);
-        iBeacon = (Button) findViewById(R.id.toIBeacon);
-        captors = (Button) findViewById(R.id.toCaptors);
+        nfc_qr = (Button) findViewById(R.id.btn_NFCandQRActivity);
+        iBeacon = (Button) findViewById(R.id.btn_IBeacon);
+        captors = (Button) findViewById(R.id.btn_Captors);
+        compass = (Button) findViewById(R.id.btn_Compass);
+
+        nfc_qr.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, LoginNFCandQRActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        iBeacon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Intent intent = new Intent(MainActivity.this, .class); // TODO: mettre le nom de la classe gérant les iBeacon
+                //startActivity(intent);
+            }
+        });
+
+        captors.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Intent intent = new Intent(MainActivity.this, .class); // TODO: mettre le nom de la classe gérant les iBeacon
+                //startActivity(intent);
+            }
+        });
+
+        compass.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, CompassActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
