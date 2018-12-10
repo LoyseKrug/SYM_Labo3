beb812511c710769b59de366c76ba2181e3bde3d
Unknown <lloysekrug@gmail.com>
2018-12-10 21:30:17 +0100

    location permission added

---
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/Services/IBeacon.java
@@ -0,0 +1,10 @@
+package ch.heigvd.iict.sym.a3dcompassapp.Services;
+
+import java.util.ArrayList;
+
+public class IBeacon {
+
+    private ArrayList<String> iBeacons = new ArrayList<String>();
+
+
+}
