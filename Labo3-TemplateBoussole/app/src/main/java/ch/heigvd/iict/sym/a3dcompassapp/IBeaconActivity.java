f041d549b82e7a82c8ea8840acd92d5d1ee164d5
Unknown <lloysekrug@gmail.com>
2018-12-11 20:34:59 +0100

    works need to add the location permission

--- Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/IBeaconActivity.java
+++ Labo3-TemplateBoussole/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/IBeaconActivity.java
@@ -1,47 +1,85 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
 
+import android.Manifest;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import org.altbeacon.beacon.*;
 
 import java.util.Collection;
+import java.util.List;
 
 public class IBeaconActivity extends AppCompatActivity implements BeaconConsumer {
 
     private TextView iBeacons = null;
     private BeaconManager beaconManager = null;
-    private BeaconParser parser = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.BLUETOOTH
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+            }
+        }).check();
+
         setContentView(R.layout.activity_ibeacon);
         iBeacons = (TextView) findViewById(R.id.beaconsList);
         beaconManager = BeaconManager.getInstanceForApplication(this);
-        parser = new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.bind(this);
     }
 
     @Override
     public void onBeaconServiceConnect() {
+
         beaconManager.addRangeNotifier(new RangeNotifier() {
 
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
-                //iBeacons.setText("");
+                iBeacons.setText("");
                 int iBeaconNumber = 0;
                 for(Beacon b : collection){
-                    String beaconInfo = "Beacon no " + iBeaconNumber +
+                    String beaconInfo = "Beacon no " + iBeaconNumber++ +
+                            "Beacon id: " + b.getId1().toString() +
                             "RSSI: " + b.getRssi() +
-                            "\nMajor: " + b.getId2().toString() +
-                            "\nMinor: " + b.getId3().toString() + "\n\n";
+                            "\nMajor number: " + b.getId2().toString() +
+                            "\nMinor number: " + b.getId3().toString() + "\n\n";
 
-                    iBeacons.setText(iBeacons.getText() + beaconInfo );
+                    iBeacons.setText(iBeacons.getText() + beaconInfo);
                 }
             }
         });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+
+        }
+
+
     }
 }
